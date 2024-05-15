f4300d76c784fbeae4903b6727d019dbf28bfb08
Sajal <123721699+rockysingh2003@users.noreply.github.com>
2024-05-15 23:08:35 +0530

    this is the more optimised version of rotation of the zeroes ....

--- Array/moving zeroes /at the end .cpp
+++ Array/moving zeroes /at the end .cpp
@@ -2,31 +2,48 @@
 using namespace std;
 void moving_zeroes(vector<int> &vr,int size)
 {
-    vector<int> br;
-    int count = 0;
+    // vector<int> br;
+    // int count = 0;
 
-    for (int i = 0; i < size;i++)
+    // for (int i = 0; i < size;i++)
+    // {
+    //     if(vr[i]!=0)
+    //     {
+    //         br.push_back(vr[i]);
+    //     }
+    //     else{
+    //         count++;
+    //     }
+    // }
+
+    // while(count!=0)
+    // {
+    //     br.push_back(0);
+    //     count--;
+    // }
+
+
+    // cout<<"\n the array after the moving the zeroes is "<<endl;
+    // for (int i = 0; i < br.size(); ++i)
+    // {
+    //     cout << br[i] << " ";
+    // }
+    // cout << endl;
+
+    int count = 0;
+    for (int i = 0; i < size; i++)
     {
         if(vr[i]!=0)
         {
-            br.push_back(vr[i]);
-        }
-        else{
-            count++;
+            swap(vr[i], vr[count]);
+            count++; //count humrha yeah bta rha hai ki kitne non -zero element apni place pe aa gye hai plus yeahh jo hai pehle zero pe point kar rha hai nad uske baad bale zero pe bhi.....
         }
     }
 
-    while(count!=0)
+    cout << "\n the elements after moving is " << endl;
+    for (int i = 0; i < size;i++)
     {
-        br.push_back(0);
-        count--;
-    }
-
-
-    cout<<"\n the array after the moving the zeroes is "<<endl;
-    for (int i = 0; i < br.size(); ++i)
-    {
-        cout << br[i] << " ";
+        cout << vr[i] << " ";
     }
     cout << endl;
 }
